fbe449b9c37835046e5f0a19c9365889a5096902
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-01-26 07:29:44 -0800

    Create the business logic for both the addContacts method and assignContacts Method

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 //  Business Logic For The AddressBook
+    //Constructor for the addressbook
 function AddressBook(){
     this.contacts = {
 
     }
+    this.contactId = 0;
 }
+
+    //Method For Creating Contact Id
+AddressBook.prototype.assignId = function(){
+    this.contactId += 1
+    return this.contactId
+}
+
+    //Method for Adding contacts
+AddressBook.prototype.addContacts = function(contact){
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+}
+
 
 // Business Logic For The Contacts 
 function Contacts(firstName , lastName , phoneNumber){
