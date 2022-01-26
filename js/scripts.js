81ed5d6b67aa360f80a0b7752e01d19545e41ff4
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-01-26 06:16:08 -0800

    Create the business logic for the contacts constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 //  Business Logic For The AddressBook
 function AddressBook(){
     this.contacts = {
-        
+
     }
+}
+
+// Business Logic For The Contacts 
+function Contacts(firstName , lastName , phoneNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+}
+Contacts.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName
 }
