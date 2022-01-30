88a3c4e22d1f1794ebdae7435742e8c0d298ab5a
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-01-30 03:31:35 -0800

    Modify your database

--- js/UIL.js
+++ js/UIL.js
@@ -1,10 +1,10 @@
 let addressBook = new AddressBook();
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
     //Owners-form
-    $("#owner-form").submit(function(event){
+    $("#owner-form").submit(function (event) {
         event.preventDefault();
         let firstName = $("#owner-firstname").val();
         let lastName = $("#owner-lastname").val();
@@ -12,17 +12,17 @@ $(document).ready(function(){
         let nationality = $("#owner-nationality").val();
         let dob = $("#owner-dob").val();
 
-        let owner = new Owner(firstName , lastName , phoneNumber , dob , nationality);
-        if(firstName === "" || lastName === "" || phoneNumber === "" || nationality === "" || dob === ""){
+        let owner = new Owner(firstName, lastName, phoneNumber, dob, nationality);
+        if (firstName === "" || lastName === "" || phoneNumber === "" || nationality === "" || dob === "") {
             $(".warn").show();
-        }else{
+        } else {
             $(".warn").hide();
             addressBook.addOwner(owner);
 
-            const fullName =  addressBook.owner[Object.keys(addressBook.owner)[0]].fullName();
+            const fullName = addressBook.owner[Object.keys(addressBook.owner)[0]].fullName();
             const ownerNationality = addressBook.owner[Object.keys(addressBook.owner)[0]].nationality;
             const ownerDob = addressBook.owner[Object.keys(addressBook.owner)[0]].dob;
-            const ownerPhone =addressBook.owner[Object.keys(addressBook.owner)[0]].phoneNumber;
+            const ownerPhone = addressBook.owner[Object.keys(addressBook.owner)[0]].phoneNumber;
 
             $("#owner-database-fullName").text(fullName)
             $("#owner-database-nationality").text(ownerNationality);
@@ -32,40 +32,60 @@ $(document).ready(function(){
             $(".form-owner").hide();
             $("#add-con").show();
         }
-        
+
 
     })
 
     //Add Button
-    $("#add").click(function(){
+    $("#add").click(function () {
         $(".database").hide();
-        $(".form-contacts").show();
-        
+        $(".form-contacts").toggle();
+
     })
 
     //Contacts Form 
-    $("#contact-form").submit(function(event){
+    $("#contact-form").submit(function (event) {
         event.preventDefault();
         let firstName = $("#contact-firstname").val();
         let lastName = $("#contact-lastname").val();
         let phoneNumber = $("#contact-phone").val();
         let nationality = $("#contact-nationality").val();
 
-        let contact = new Contacts(firstName , lastName , phoneNumber , nationality);
-        if(firstName === "" || lastName === "" || phoneNumber === "" || nationality === "" ){
+        let contact = new Contacts(firstName, lastName, phoneNumber, nationality);
+        if (firstName === "" || lastName === "" || phoneNumber === "" || nationality === "") {
             $(".warn").show();
-        }else{
+        } else {
             $(".warn").hide();
             addressBook.addContacts(contact);
             $(".form-contacts").hide();
-                    }
-        
+        }
+
 
     })
-    $("#database").click(function(){
+
+    //Database Click Method
+    $("#database").click(function () {
         $(".form-contacts").hide();
-        $(".database").show();
+        $(".database").toggle();
+
     })
+
+    
+    $("#select-btn").click(function(){
+        const databaseValue = $("#database-select").find(":selected").text();
+    if (databaseValue == "Owner"){
+        $(".my-contact-database").hide();
+        $(".owner-database").show();
+    }else{
+        $(".owner-database").hide();
+        $(".my-contact-database").show();
+    }
+        
+
+
+    })
+
+
 
 })
 
